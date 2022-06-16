a73654dae9493e4f782e0064351b10659bd7a4a8
Priya-shan <Shanmugapriyashanu2002@gmail.com>
2022-06-16 21:11:20 +0530

    initial-commit

--- app/src/main/java/com/example/mini_project/MainActivity4.java
+++ app/src/main/java/com/example/mini_project/MainActivity4.java
@@ -1,12 +1,25 @@
 package com.example.mini_project;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity4 extends AppCompatActivity {
+    Button cse;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main4);
+        cse=findViewById(R.id.cse);
+        cse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent ii=new Intent(MainActivity4.this,cgpa_calculator.class);
+                startActivity(ii);
+                finish();
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/mini_project/cgpa_calculator.java
@@ -0,0 +1,14 @@
+package com.example.mini_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class cgpa_calculator extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cgpa_calculator);
+    }
+}
